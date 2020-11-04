11878f3a00534b00dd4d906175b3ad9e8801ad7e
Artem <klaid-87@mail.ru>
2020-11-04 20:25:16 +0300

    Начал стилизацию header и features

--- src/js/script.js
+++
@@ -1,6 +0,0 @@
-gulp.task("html", function() {
-  return gulp.src("src/*.html")
-    .pipe(gulpif(!isDevelopment, replace("/style", "/style.min")))
-    .pipe(gulpif(!isDevelopment, replace("/script", "/script.min")))
-    .pipe(gulp.dest("build"))
-});
